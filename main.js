109798d822c015928b22337289f5bc1f66cf2450
RPAAKA <65394319+RPAAKA@users.noreply.github.com>
2020-07-13 16:34:05 +0900

    JS home contact btn

--- main.js
+++ main.js
@@ -22,7 +22,16 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  scrollIntoView(link);
 });
+
+//handle click on "contact me" button on home
+const homecontactbtn = document.querySelector('.home_contact');
+homecontactbtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
