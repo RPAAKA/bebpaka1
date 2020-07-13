ce05a2871b6df3fbb6c4cc8535441ee04d9963c8
RPAAKA <65394319+RPAAKA@users.noreply.github.com>
2020-07-13 16:08:02 +0900

    JS navbar menu click

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+//handle scrolling when tapping on the navbar menu -> javascript scroll to id 로 검색
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
